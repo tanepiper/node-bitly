11b7d03b2083b9f2ba4773678592d2966b0f4de2
Tane Piper <piper.tane@gmail.com>
2011-06-03 22:51:16 +0100

    Started on re-write for fixes

--- tests/shorten.js
+++ tests/shorten.js
@@ -1,7 +1,7 @@
 var util = require('util');
-var Bitly = require(__dirname +'/../lib/bitly/Bitly').Bitly;
+var Bitly = require(__dirname +'/..').Bitly;
 console.log('Running test: Shorten Url');
-var bitly = new Bitly('<YOUR USERNAME>', '<YOUR API KEY>');
+var bitly = new Bitly({login:'bitlynodejs', api_key:'R_8a2a91d31932dc7fda5468033dfe3c15'});
 
 bitly.shorten('http://tanepiper.com', function(result) {
   console.log(util.inspect(result));
