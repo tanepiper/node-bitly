9c77fa55d564ee2e23f00dec498060cb792aa0a6
Tane Piper <piper.tane@gmail.com>
2017-10-30 22:19:05 +0100

    Working on getting requests going

--- src/lib.js
+++ src/lib.js
@@ -1,32 +1,54 @@
 const url = require('url');
-const http = require('http');
+const http = require('https');
 const isUri = require('valid-url').isUri;
 
 /**
-   * Generates the URL object to be passed to the HTTP request for a specific
-   * API method call
-   * @param {object} options The options to be passed to the method
-   * @param {string} accessToken The access token to be passed
-   * @param {string} [apiUrl=api-ssl.bitly.com] The API url to call against
-   * @param {string} [apiVersion=v3] apiVersion The API version being called
-   * @param {string} method The Bit.ly method to call with the request
-   * @param {object} query Query object to pass to the API
-   * @return {Object} The URL object for this request
-   */
-const generateNiceUrl = ({ accessToken, method, apiUrl = 'api-ssl.bitly.com', apiVersion = 'v3', query = {} }) =>
-    url.parse(
+ * 
+ * @param {object} UrlParameters An object of paramters to pass to generate a bit.ly url
+ * @param {string} accessToken Your bit.ly access token
+ * @param {string} method The method to call
+ * @param {object} A data object specifying bit.ly keys for your method
+ * 
+ * @example
+ * generateUrl({method: 'shorten', accessKey: 'myaccessKey', data: { longUrl: 'https://github.com/tanepiper/node-bitly' } });
+ */
+const generateUrl = ({
+    accessToken,
+    method,
+    data = {},
+    apiUrl = 'api-ssl.bitly.com',
+    apiVersion = 'v3',
+    domain = 'bit.ly',
+    format = 'json',
+    query = {}
+}) => {
+    const keys = Object.keys(data);
+
+    const newQuery = Object.assign(
+        {
+            access_token: accessToken,
+            domain,
+            format
+        },
+        query
+    );
+
+    keys.forEach(key => (newQuery[key] = data[key]));
+
+    return url.parse(
         url.format({
             protocol: 'https',
             hostname: apiUrl,
             pathname: `/${apiVersion}/${method}`,
-            query: { access_token: accessToken, ...query }
+            query: newQuery
         })
     );
+};
 
-const doRequest = async ({ url }) => {
+const doRequest = async ({ uri }) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const req = await http.get(url);
+            const req = await http.get(uri);
             req.on('response', res => {
                 resolve(res);
             });
@@ -41,53 +63,45 @@ const doRequest = async ({ url }) => {
     });
 };
 
-const doMethod = async ({ method, accessToken, data, domain, format }) => {
-    const keys = Object.keys(data);
-    const query = {
-        access_token: accessToken,
-        domain,
-        format
-    };
-    keys.forEach(key => (query[key] = data[key]));
-    return await doRequest(generateNiceUrl({ query, method }));
-};
+// const doMethod = async ({ method, accessToken, data, domain, format }) => {
+//     return await doRequest(generateUrl({ query, method }));
+// };
 
-/**
-* Function to check through an array of items to check for short urls or hashes
-* @param  {Array} items The array of items to be checked
-* @param  {Object} query The query object
-* @return {void}
-*/
-const sortUrlsAndHash = ({ items = [], query: {} }) => {
-    const shortUrl = [];
-    const hash = [];
+// /**
+// * Function to check through an array of items to check for short urls or hashes
+// * @param  {Array} items The array of items to be checked
+// * @param  {Object} query The query object
+// * @return {void}
+// */
+// const sortUrlsAndHash = ({ items = [], query: {} }) => {
+//     const shortUrl = [];
+//     const hash = [];
 
-    // If only passed one item, put in array for url checking
-    if (typeof items === 'string') {
-        items = [items];
-    }
-    items.forEach(item => (isUri(item) ? shortUrl.push(item) : hash.push(item)));
+//     // If only passed one item, put in array for url checking
+//     if (typeof items === 'string') {
+//         items = [items];
+//     }
+//     items.forEach(item => (isUri(item) ? shortUrl.push(item) : hash.push(item)));
 
-    if (shortUrl.length > 0) query.shortUrl = shortUrl;
-    if (hash.length > 0) query.hash = hash;
-};
+//     if (shortUrl.length > 0) query.shortUrl = shortUrl;
+//     if (hash.length > 0) query.hash = hash;
+// };
 
-const bitly = async (
-    { accessToken, domain, format } = {
-        format: 'json',
-        domain: 'bit.ly'
-    }
-) => {
-    const bitlyInstance = { accessToken, domain, format };
-};
+// const bitly = async (
+//     { accessToken, domain, format } = {
+//         format: 'json',
+//         domain: 'bit.ly'
+//     }
+// ) => {
+//     const bitlyInstance = { accessToken, domain, format };
+// };
 
 /**
  * @module node-bitly.lib
- * @exports { generateNiceUrl }
+ * @exports { generateUrl }
  */
+
 module.exports = {
-    generateNiceUrl,
+    generateUrl,
     doRequest,
-    doMethod,
-    bitly
 };
--- src/lib.spec.js
+++ src/lib.spec.js
@@ -1,28 +1,75 @@
 const expect = require('chai').expect;
 
-const { generateNiceUrl, doRequest, doMethod } = require('../src/lib');
+process.on('unhandledRejection', err => {
+    /*eslint-disable */
+    console.log(err.stack);
+    process.exit(1);
+    /*eslint-enable */
+});
 
-describe('generateNiceUrl', () => {
+process.on('uncaughtException', error => {
+    /*eslint-disable */
+    console.log(error.stack); // to see your exception details in the console
+    process.exit(1);
+    /*eslint-enable */
+});
+
+const { generateUrl, doRequest } = require('../src/lib');
+
+describe('generateUrl', () => {
     it('should return a default url', () => {
-        const result = generateNiceUrl({ accessToken: 'iamatoken', method: 'foo' });
-        expect(result.href).to.equal('https://api-ssl.bitly.com/v3/foo?access_token=iamatoken');
+        const result = generateUrl({ accessToken: 'iamatoken', method: 'foo' });
+        expect(result.href).to.equal(
+            'https://api-ssl.bitly.com/v3/foo?access_token=iamatoken&domain=bit.ly&format=json'
+        );
     });
 
     it('should return a custom url', () => {
-        const result = generateNiceUrl({
+        const result = generateUrl({
             accessToken: 'iamatoken',
             method: 'foo',
             apiUrl: 'api-ssl.myhost.com',
             apiVersion: 'v4'
         });
-        expect(result.href).to.equal('https://api-ssl.myhost.com/v4/foo?access_token=iamatoken');
+        expect(result.href).to.equal(
+            'https://api-ssl.myhost.com/v4/foo?access_token=iamatoken&domain=bit.ly&format=json'
+        );
     });
 });
 
 describe('doRequest', () => {
-    it('makes requests', async () => {
-        const result = await doRequest({ url: 'http://example.com' });
+    before(() => {});
+
+    it('makes a request with a generateUrl url', async (done) => {
+        const uri = generateUrl({ accessToken: process.env.BITLY_API_KEY, method: 'shorten', data: { longUrl: 'http://example.com'} });
+        const result = await doRequest({ uri: uri.href });
         const { statusCode } = result;
+
+        const data = [];
+        result.on('data', data => {
+          console.log(data);
+        });
+        result.on('end', () => {
+          expect(data.join('')).to.equal('')
+        });
         expect(statusCode).to.equal(200);
     });
 });
+
+// describe('generateQuery', () => {
+//     it('should generate a querypart for a request', () => {
+//         const result = generateQuery({ accessToken: 'foobar', data: { foo: 'bar', moo: [1, 2] } });
+//         expect(result).to.deep.equal({
+//             access_token: 'foobar',
+//             foo: 'bar',
+//             moo: [1, 2],
+//             domain: 'bit.ly',
+//             format: 'json'
+//         });
+//     });
+
+//     it('should generate a nice url', () => {
+//         const query = generateQuery({ accessToken: 'foobar', data: { foo: 'bar', moo: [1, 2] } });
+//         const result = generateNiceUrl({ accessToken: 'iamatoken', method: 'foo', query });
+//     });
+// });
