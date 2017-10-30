e6ced3ea6cb0775c800c17126ee16d292fccebc4
Tane Piper <piper.tane@gmail.com>
2017-10-30 22:39:39 +0100

    Add promise exception code to boostrap file

--- src/lib.spec.js
+++ src/lib.spec.js
@@ -1,18 +1,6 @@
 const expect = require('chai').expect;
 
-process.on('unhandledRejection', err => {
-    /*eslint-disable */
-    console.log(err.stack);
-    process.exit(1);
-    /*eslint-enable */
-});
-
-process.on('uncaughtException', error => {
-    /*eslint-disable */
-    console.log(error.stack); // to see your exception details in the console
-    process.exit(1);
-    /*eslint-enable */
-});
+require('../test/bootstrap');
 
 const { generateUrl, doRequest } = require('../src/lib');
 
