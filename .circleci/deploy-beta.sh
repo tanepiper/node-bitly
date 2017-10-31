60954a00522af24f040c3516d074fd3e1ca45f07
Tane Piper <tanepiper@users.noreply.github.com>
2017-10-31 20:50:01 +0100

    Pipeline for release (#27)
    * Test pipeline
    
    * More pipeline improvements
    
    * Deploy beta pipeline
    
    * fix error
    
    * Move branch filter
    
    * just use node 8
    
    * Hopefully fix config
    
    * Try get package version
    
    * Fix yaml
    
    * Point to bash file
    
    * Add git config
    
    * Add npm token
    
    * Add prod ci
    
    * Only release patch on version
    
    * Add tag release for beta
    
    * Add ssh key fingerprint for release
    
    * Indent
    
    * Prepare for master

---
+++ .circleci/deploy-beta.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+echo "Doing NPM Beta Release"
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+git config --global user.name $CIRCLE_USERNAME
+git config --global user.email piper.tane@gmail.com
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
+npm version $PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM -m "beta release %s"
+npm publish --tag beta
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && git tag $PACKAGE_VERSION && git push --tags
+
+echo "Release done"
---
+++ .circleci/deploy-production.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+echo "Doing NPM Release"
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+git config --global user.name $CIRCLE_USERNAME
+git config --global user.email piper.tane@gmail.com
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+
+npm version patch -m "node-bitly %s"
+npm publish
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && git tag $PACKAGE_VERSION && git push --tags
+
+echo "Release done"
