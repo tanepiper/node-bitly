b3d6d8c068c54b9c0f8e7514101ffa6d6e41246d
Tane Piper <piper.tane@gmail.com>
2017-10-31 19:56:06 +0100

    Add git config

--- .circleci/deploy-beta.sh
+++ .circleci/deploy-beta.sh
@@ -11,6 +11,9 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
+git config --global user.name $CIRCLE_USERNAME
+git config --global user.email piper.tane@gmail.com
+
 npm version $PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM -m "beta release %s"
 npm publish --tag beta
 
