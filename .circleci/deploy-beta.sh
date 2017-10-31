a8e0f1353e85f54b9a75ed30e06166002e7b21ba
Tane Piper <piper.tane@gmail.com>
2017-10-31 22:28:03 +0100

    More sacrifices to the git gods

--- .circleci/deploy-beta.sh
+++ .circleci/deploy-beta.sh
@@ -11,15 +11,21 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
+NEW_PACKAGE_VERSION=$PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM
+
 git config --global push.default simple
 git config --global user.name $CIRCLE_USERNAME
 git config --global user.email piper.tane@gmail.com
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
-npm version $PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM -m "$CIRCLE_BRANCH %s"
+npm version NEW_PACKAGE_VERSION -m "$CIRCLE_BRANCH %s"
 npm publish --tag beta
 
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && git tag $PACKAGE_VERSION && git push --tags && git push --set-upstream origin $CIRCLE_BRANCH
+git add .
+git commit -m "$NEW_PACKAGE_VERSION"
+git tag $NEW_PACKAGE_VERSION
+git push --tags
+git push --set-upstream origin $CIRCLE_BRANCH
 
 echo "Release done"
