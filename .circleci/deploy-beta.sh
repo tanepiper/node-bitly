3ddc0fb979d6f28894ab0f15923178a3bb17c91c
Tane Piper <piper.tane@gmail.com>
2017-10-31 22:22:02 +0100

    Satisfy the git gods

--- .circleci/deploy-beta.sh
+++ .circleci/deploy-beta.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o noclobber -o nounset -o pipefail
 
-echo "Doing NPM Beta Release"
+echo "Doing NPM Beta Release branch: $CIRCLE_BRANCH"
 
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
@@ -11,14 +11,15 @@ PACKAGE_VERSION=$(cat package.json \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 
+git config --global push.default simple
 git config --global user.name $CIRCLE_USERNAME
 git config --global user.email piper.tane@gmail.com
 
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
-npm version $PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM -m "beta release %s"
+npm version $PACKAGE_VERSION-beta-$CIRCLE_BUILD_NUM -m "$CIRCLE_BRANCH %s"
 npm publish --tag beta
 
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && git tag $PACKAGE_VERSION && git push --tags && git push
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') && git tag $PACKAGE_VERSION && git push --tags && git push --set-upstream origin $CIRCLE_BRANCH
 
 echo "Release done"
