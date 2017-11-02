321dca519b2508251bc1b5a142768cc1733123c3
Tane Piper <piper.tane@gmail.com>
2017-11-02 13:59:04 +0100

    Fix folder docs

--- .circleci/build-dist.sh
+++ .circleci/build-dist.sh
@@ -6,6 +6,12 @@ echo "Building Distribution"
 npm run build
 
 echo "Building new docs"
+DIR=$RUNNING_DIR/docs
+if [ -d "$DIR" ]; then
+    printf '%s\n' "Removing existing docs"
+    rm -rf "$DIR"
+fi
+mkdir $RUNNING_DIR/docs
 npm run ghdocs
 
 echo "Build Done"
