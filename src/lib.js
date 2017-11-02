673b9a7f9996d39fe727f08ab0d61ba8ee604c2a
Tane Piper <tanepiper@users.noreply.github.com>
2017-11-02 13:33:22 +0100

    Proper async/await error handling and improved API docs (#32)
    * Correctly handle errors in public apis, add new fixtures
    * Proper async test
    * More test and general improvements
    * Update ignore don't include config in npm
    * Fix folder paths for workspace
    * Persist build and docs

--- src/lib.js
+++ src/lib.js
@@ -3,11 +3,19 @@ const request = require('request-promise');
 const isUri = require('valid-url').isUri;
 
 /**
- *
+ * The internal library of node-bitly
+ * @module node-bitly.lib
+ * @private
+ */
+
+/**
+ * Generates a valid URL for a GET request to the Bit.ly API
  * @param {object} UrlParameters An object of paramters to pass to generate a bit.ly url
  * @param {string} accessToken Your bit.ly access token
  * @param {string} method The method to call
- * @param {object} A data object specifying bit.ly keys for your method
+ * @param {object} data a data object specifying bit.ly keys for your method
+ * @param {object} config A custom config object to overide values
+ * @private
  *
  * @example
  * generateUrl({method: 'shorten', accessKey: 'myaccessKey', data: { longUrl: 'https://github.com/tanepiper/node-bitly' } });
@@ -15,7 +23,7 @@ const isUri = require('valid-url').isUri;
 const generateUrl = (
   accessToken,
   method,
-  data = {},
+  data,
   { apiUrl = 'api-ssl.bitly.com', apiVersion = 'v3', domain = 'bit.ly', format = 'json' } = {},
 ) => {
   const newQuery = Object.assign({
@@ -25,7 +33,6 @@ const generateUrl = (
   });
 
   Object.keys(data || []).forEach(key => (newQuery[key] = data[key]));
-  //console.log(newQuery);
 
   return url.parse(
     url.format({
@@ -37,31 +44,37 @@ const generateUrl = (
   );
 };
 
-const doRequest = async ({ accessToken, config, method, data }) => {
+/**
+ * Method called to generate a url and call the request
+ * @param {object} options The options object
+ * @param {string} options.accessToken The request accessToken
+ * @param {string} options.method The method to be called on Bitly
+ * @param {object} options.data A data object with key=>value pairs mapped to request parameters
+ * @param {config} options.config A object that overrides the default values for a request
+ * @returns {object} The request result object
+ */
+const doRequest = async ({ accessToken, method, data, config }) => {
   const uri = generateUrl(accessToken, method, data, config);
-  //console.log(uri);
   try {
     const req = await request({ uri });
     return JSON.parse(req);
-  } catch (e) {
-    console.log('Request Failed');
-    throw e;
+  } catch (error) {
+    throw error;
   }
 };
 
 /**
 * Function to check through an array of items to check for short urls or hashes
 * If only passed one item, put in array for url checking
-* @param  {Array} unsortedItems The array of items to be checked
-* @param  {Object} query The query object
-* @return {void}
+* @param  {Array<string>} unsortedItems The array of items to be checked
+* @param  {object} query The query object
+* @return {object}
 */
 const sortUrlsAndHash = (unsortedItems, result = { shortUrl: [], hash: [] }) => {
   (Array.isArray(unsortedItems) ? unsortedItems : [unsortedItems]).map(
     item =>
       isUri(item) ? result.shortUrl.push(item) : typeof item === 'string' && result.hash.push(item),
   );
-  //console.log(result);
   return result;
 };
 
