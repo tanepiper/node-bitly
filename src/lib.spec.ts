1ab05c213135af1533fe0ebdbc31474adf5adf03
Tane Piper <piper.tane@gmail.com>
2018-06-13 15:33:08 +0200

    Update tests

--- src/lib.spec.ts
+++ src/lib.spec.ts
@@ -7,7 +7,9 @@ import { generateUrl, sortUrlsAndHash } from './lib';
 describe('generateUrl', () => {
   it('should return a default url', () => {
     const result = generateUrl('iamatoken', 'foo');
-    expect(result.href).to.equal('https://api-ssl.bitly.com/v3/foo?access_token=iamatoken&domain=bit.ly&format=json');
+    expect(result)
+      .to.have.property('href')
+      .and.to.equal('https://api-ssl.bitly.com/v3/foo?access_token=iamatoken&domain=bit.ly&format=json');
   });
 
   it('should return a custom url', () => {
@@ -15,14 +17,16 @@ describe('generateUrl', () => {
       apiUrl: 'api-ssl.myhost.com',
       apiVersion: 'v3'
     });
-    expect(result.href).to.equal('https://api-ssl.myhost.com/v3/foo?access_token=iamatoken&domain=bit.ly&format=json');
+    expect(result)
+      .to.have.property('href')
+      .and.to.equal('https://api-ssl.myhost.com/v3/foo?access_token=iamatoken&domain=bit.ly&format=json');
   });
 });
 
 describe('sortUrlsAndHash', () => {
   it('takes urls and hashes and appends them correctly', () => {
     const { shortUrl, hash } = sortUrlsAndHash(['http://example.com', '1KjIwXl']);
-    expect(shortUrl.length).to.equal(1);
-    expect(hash.length).to.equal(1);
+    expect(shortUrl).to.have.lengthOf(1);
+    expect(hash).to.have.lengthOf(1);
   });
 });
