4592162bfc038c952b2b5e4f7c2e66896a3055c3
Tane Piper <piper.tane@gmail.com>
2018-06-13 15:29:41 +0200

    Add new pipeline

--- .circleci/deploy-production.sh
+++ .circleci/deploy-production.sh
@@ -2,6 +2,8 @@
 
 set -o errexit -o noclobber -o nounset -o pipefail
 
+VERSION_NUMBER=$(echo "$CIRCLE_BRANCH" | sed 's/[^0-9]*//g')
+
 echo "Doing NPM Release"
 
 PACKAGE_VERSION=$(grep -m1 version package.json | sed -E 's/.*"(([0-9]+\.?)+).*/\1/')
@@ -16,7 +18,14 @@ git add .
 git diff-index --quiet HEAD || git commit -m "Commit changes for $PACKAGE_VERSION"
 
 npm version ${VERSION_COMMAND:-patch} -m "$CIRCLE_BRANCH %s [ci skip]"
-npm publish
+
+if [[ "5" -eq "$VERSION_NUMBER" ]] ;
+then
+  npm publish --tag stable
+else
+  npm publish --tag latest
+fi
+
 git push --tags
 git push --set-upstream origin $CIRCLE_BRANCH
 
