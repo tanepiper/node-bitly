6e1a3449826d2a30438fbd59b9a9eed495f6dbe3
Tane Piper <tane@backbase.com>
2019-06-14 10:26:18 +0200

    Working on fixing types

--- src/bitly.types.ts
+++ src/bitly.types.ts
@@ -1,40 +1,115 @@
+/**
+ * A Bitly module configuration
+ */
 export interface BitlyConfig {
+  /**
+   * The API URL, optional as default is provided
+   */
   apiUrl?: string;
-  apiVersion?: 'v3'; // Fixed as we only support this version for now
+  /**
+   * API Version, currently v3 is only supported
+   */
+  apiVersion?: "v3"; // Fixed as we only support this version for now
+  /**
+   * Optional custom domain
+   */
   domain?: string;
+  /**
+   * Return format, default is JSON
+   */
   format?: string;
   [k: string]: any;
 }
 
+/**
+ * Paramaters that can be passed to a Bitly URL request
+ */
 export interface BitlyUrlQueryParams {
+  /**
+   * A list of short url hashes
+   */
   shortUrl?: string[];
+  /**
+   * A list of long URLS
+   */
   longUrl?: string;
+  /**
+   * A single URL
+   */
   url?: string;
+  /**
+   * A single hash
+   */
   hash?: string[];
   [k: string]: any;
 }
 
+/**
+ * The response from a shorten request
+ */
 export interface ShortenResponse {
+  /**
+   * The Bitly URL
+   */
   url: string;
+  /**
+   * The bitly hash
+   */
   hash: string;
+  /**
+   * Global hash if URL already available
+   */
   global_hash: string;
+  /**
+   * Long URL for this response
+   */
   long_url: string;
+  /**
+   * If it's a new hash or not
+   */
   new_hash: number;
 }
 
-export interface ExpandInteface {
+/**
+ * An expand response
+ */
+export interface ExpandResponse {
   expand: string[];
 }
 
-export type BitlyResponseData = ShortenResponse | ExpandInteface;
+/**
+ * Type for a response from Bitly
+ */
+export type BitlyResponseData = ShortenResponse | ExpandResponse;
 
-export interface BitlyResponse {
+/**
+ * Type for a bitly response
+ */
+export interface BitlyResponse<T = BitlyResponseData> {
+  /**
+   * Status code of the response
+   */
   status_code: number;
+  /**
+   * The status text
+   */
   status_txt: string;
-  data: BitlyResponseData;
+  /**
+   * The data being returned
+   */
+  data: T;
 }
 
-export interface BitlyError extends Error {
+/**
+ * An error from Bitly
+ */
+export interface BitlyError<T = any> extends Error {
+  /**
+   * Status code of the error
+   */
   statusCode: number;
-  data: object;
+  /**
+   * Error data
+   */
+  data: T;
 }
